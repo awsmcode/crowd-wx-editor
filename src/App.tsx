25bae7b2ec122b60a115d9276bde26cbd99dc86b
AwsmCode <awsmcode@gmail.com>
2025-06-05 13:25:11 +0200

    minor improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,86 @@
 import { useState } from 'react'
 import './App.css'
 
-
-function App() {
-  const [panelIndex, setPanelIndex] = useState(0);
-
-  function goToPanel(idx) {
-    setPanelIndex(idx);
-  }
-
-  function nextPanel() {
-    if (panelIndex < 3) setPanelIndex(panelIndex + 1);
-  }
-
-  function prevPanel() {
-    if (panelIndex > 0) setPanelIndex(panelIndex - 1);
-  }
-
-  return (
-    <div className="slider-container">
-    <div
-      className="slider-inner"
-      style={{
-        transform: `translateX(-${panelIndex * 100}vw)`,
-      }}
-    >
-      <div className="panel panel1">
-        <PanelContent
-          text="Kategorien"
-          onNext={nextPanel}
-          onPrev={prevPanel}
-          showPrev={false}
-          showNext={true}
-        />
-      </div>
-      <div className="panel panel2">
-        <PanelContent
-          text="Ausprägungen"
-          onNext={nextPanel}
-          onPrev={prevPanel}
-          showPrev={true}
-          showNext={true}
-        />
-      </div>
-      <div className="panel panel3">
-        <PanelContent
-          text="Zeit und Standort"
-          onNext={nextPanel}
-          onPrev={prevPanel}
-          showPrev={true}
-          showNext={true}
-        />
-      </div>
-      <div className="panel panel4">
-        <PanelContent
-          text="Vielen Dank!"
-          onNext={nextPanel}
-          onPrev={prevPanel}
-          showPrev={true}
-          showNext={false}
-        />
-      </div>
-    </div>
-
-  </div>
-);
+export type Location = {
+    name: string;
+    lng: number;
+    lat: number;
 }
 
+function App({ locations }: { locations: Location[] }) {
+    const [panelIndex, setPanelIndex] = useState(0);
 
+    function goToPanel(idx: number) {
+      setPanelIndex(idx);
+    }
 
-function PanelContent({ text, onNext, onPrev, showPrev, showNext }: { text: string, onNext: () => void, onPrev: () => void, showPrev: boolean, showNext: boolean }) {
+    function nextPanel() {
+        if (panelIndex < 3) setPanelIndex(panelIndex + 1);
+    }
+
+    function prevPanel() {
+        if (panelIndex > 0) setPanelIndex(panelIndex - 1);
+    }
+
+    return (
+        <div className="slider-container">
+            <div
+                className="slider-inner"
+                style={{
+                    transform: `translateX(-${panelIndex * 25}%)`,
+                }}
+            >
+                <div className="panel panel1">
+                    <PanelContent
+                        text={JSON.stringify(locations)}
+                        onNext={nextPanel}
+                        onPrev={prevPanel}
+                        showPrev={false}
+                        showNext={true}
+                    />
+                </div>
+                <div className="panel panel2">
+                    <PanelContent
+                        text="Ausprägungen"
+                        onNext={nextPanel}
+                        onPrev={prevPanel}
+                        showPrev={true}
+                        showNext={true}
+                    />
+                </div>
+                <div className="panel panel3">
+                    <PanelContent
+                        text="Zeit und Standort"
+                        onNext={nextPanel}
+                        onPrev={prevPanel}
+                        showPrev={true}
+                        showNext={true}
+                    />
+                </div>
+                <div className="panel panel4">
+                    <PanelContent
+                        text="Vielen Dank!"
+                        onNext={nextPanel}
+                        onPrev={prevPanel}
+                        showPrev={true}
+                        showNext={false}
+                    />
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+type TPanelContentProps = {
+  text: string;
+  onNext: () => void;
+  onPrev: () => void;
+  showPrev: boolean;
+  showNext: boolean;
+}
+
+function PanelContent({ text, onNext, onPrev, showPrev, showNext }: TPanelContentProps) {
     return (
         <>
             <h2>{text}</h2>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App, { type Location } from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = document.getElementById('cw-root');
+
+if (root) {
+    const locationsJson = root.getAttribute('data-locations');
+
+    let locations: Location[] = [];
+    if (locationsJson) {
+
+        try {
+            locations = JSON.parse(locationsJson);
+            // Jetzt kannst du mit locations weiterarbeiten!
+            console.log(locations);
+        } catch (e) {
+            console.error("Locations-Parsing-Fehler:", e);
+        }
+    }
+
+    createRoot(root!).render(
+        <StrictMode>
+          <App locations={locations} />
+        </StrictMode>,
+    )
+
+}
